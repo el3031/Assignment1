f283927368101745fbc34d715ec65eeed3c8021c
el3031 <67562724+el3031@users.noreply.github.com>
2022-02-02 14:11:47 -0500

    basic move in direction on click implemented. havent tried it on phone yet. also haven't added any rotate towards features

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,25 +7,36 @@ using UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+
     void Start()
     {
-        TouchSimulation.Enable();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            Touch touch = Input.GetTouch(0);
-
-            // Update the Text on the screen depending on current position of the touch each frame
-            Debug.Log("Touch Position : " + touch.position);
-        }
-        else
-        {
-            Debug.Log("No touch contacts");
+            Vector3 mousePos = Input.mousePosition;
+            //Debug.Log(Input.mousePosition);
+            Ray onScreenPoint = Camera.main.ScreenPointToRay(mousePos);
+            Debug.Log(onScreenPoint);
+            
+            RaycastHit hit;
+            if (Physics.Raycast(onScreenPoint.origin, onScreenPoint.direction, out hit))
+            {
+                Vector3 loc = hit.point;
+                rb.MovePosition(transform.position + loc * Time.deltaTime * 10f);
+                
+            }
+            
+            /*
+            Vector3 newPosition = transform.position + onScreenPoint.direction * 10f;
+            rb.MovePosition(transform.position + newPosition * Time.deltaTime);
+            */
+            Debug.DrawRay(onScreenPoint.origin, onScreenPoint.direction * 10f, Color.red, 10f);
         }
     }
 }
